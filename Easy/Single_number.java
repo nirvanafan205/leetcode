211d079e242ce1f46dad431a0b914ef8f096db99
Matthew <mgarcia495@yahoo.com>
2020-06-19 17:02:27 -0700

    testing

---
+++ Easy/Single_number.java
@@ -0,0 +1,38 @@
+public class Single_number
+{
+	public int singleNumber(int[] nums)
+	{
+		int target;
+
+		for(int i = 0; i < nums.length; i++)
+		{
+			int count = 0;
+
+			for(int j = 0; j < nums.length; j++)
+			{
+				if(nums[i] == nums[j])
+				{
+					count++;
+				}
+			}
+
+			if(count == 1)
+			{
+				target = nums[i];
+
+				return target;
+			}
+		}
+
+		throw new IllegalArgumentException("no match");
+	}
+
+	public static void main(String[] args)
+	{
+		int array[] = { 2, 2, 5, 2, 2 };
+
+		Single_number caller = new Single_number();
+
+		caller.singleNumber(array);
+	}
+}
