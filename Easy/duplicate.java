490a82fd502375ddcd0ffb7f80519b90541ec5e2
nirvanafan205 <47827017+nirvanafan205@users.noreply.github.com>
2020-07-05 22:44:38 -0700

    Update duplicate.java

--- Easy/duplicate.java
+++ Easy/duplicate.java
@@ -1,3 +1,7 @@
+//Given an array of integers, find if the array contains any duplicates.
+//our function should return true if any value appears at least twice in the array, 
+//and it should return false if every element is distinct.
+
 import java.util.Arrays;
 
 public class duplicate
