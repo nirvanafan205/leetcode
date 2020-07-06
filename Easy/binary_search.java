cfe2ba08f064a39ca9dd8873b3709821afbacc5a
nirvanafan205 <47827017+nirvanafan205@users.noreply.github.com>
2020-07-05 22:43:19 -0700

    Update binary_search.java

--- Easy/binary_search.java
+++ Easy/binary_search.java
@@ -1,3 +1,6 @@
+//Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+//write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
 public class binary_search
 {
 	public int search(int nums[], int target)
