5f0520a745a0bc1061c181435e4ebbd0a268758a
Matthew <mgarcia495@yahoo.com>
2021-07-25 14:32:21 -0700

    fix evens

--- c++/diagnal_matrix.cpp
+++ c++/diagnal_matrix.cpp
@@ -4,42 +4,63 @@ using namespace std;
 
 int main()
 {
-	vector<vector<int>> nums { {1, 2, 3} , {4, 5, 6} , {7, 8, 9} };
-	int rows = nums.size();
-	int columns = nums[0].size();
+	vector<vector<int>> mat {{10,16,1,7,20,5,2,15},{10,4,14,4,1,1,15,9},{5,14,14,13,1,8,14,2},{12,1,10,12,8,16,2,14},{1,10,9,15,19,10,8,1},{20,19,13,16,5,5,15,16},{17,15,9,10,10,14,4,1},{7,16,14,2,3,1,5,18}}; 
+
+	int rows = mat.size();
+	int columns = mat[0].size();
 	int sum = 0;
 
-	for(int i = 0; i < rows; i++)
+	if(columns % 2 == 1)
 	{
-		for(int b = 0; b < columns; b++)
+		for(int i = 0; i < rows; i++)
 		{
-			if(i == 0 && b == 0)
+			for(int b = 0; b < columns; b++)
 			{
-				sum += nums[i][b];
-			}
+				if((i == 0 || i ==  rows - 1) && (b == 0 || b == columns - 1) )
+				{
+					//sum += mat[i][b];
+					cout << mat[i][b] << endl;
+				}
+				
+				else if((i == 1 || i == rows - 2) && (b == 1 || b == columns - 2) )
+				{
+					//sum += mat[i][b];
+					cout << mat[i][b] << endl;
+				}
 
-			else if(i == 0 && b == columns - 1)
-			{
-				sum += nums[i][b];
+				else if( (i == rows / 2) && (b == columns / 2) )
+				{
+					cout << mat[i][b] << endl;
+				}
 			}
-
-			else if(i == rows - 1 && b == 0)
-			{
-				sum += nums[i][b];
-			}
-
-			else if(i == rows-1 && b == columns - 1)
-			{
-				sum += nums[i][b];
-			}
-
-			cout << nums[i][b] << " ";
 		}
-
-		cout << endl;
 	}
 
-	cout << endl;
+	else if(columns % 2 == 0)
+	{
+		for(int i = 0; i < rows; i++)
+		{
+			for(int b = 0; b < columns; b++)
+			{
+				if((i == 0 || i == rows - 1) && (b == 0 || b == columns - 1) )
+				{
+					sum += mat[i][b];
+					cout << mat[i][b] << endl;
+				}
 
-	cout << sum << endl;
+				else if((i == 1 || i == rows - 2) && (b == 1 || b == columns - 2) )
+				{
+					sum += mat[i][b];
+					cout << mat[i][b] << endl;
+				}
+
+				else if( (i > 1 && i < rows - 2) && (b > 1 && b < columns - 2) )
+				{
+					sum += mat[i][b];	
+					cout << mat[i][b] << endl;
+				}
+			}
+		}
+	}
+
 }
