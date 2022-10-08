6912a14b7e06e4fdf04f14ecc5448b68b825def1
nirvanafan205 <nirvanafan205@gmail.com>
2022-10-08 12:14:25 -0700

    reviewed two sum

--- java/grindTime/twoSum.java
+++ java/grindTime/twoSum.java
@@ -2,13 +2,18 @@ public class twoSum
 {
 	public int[] twoSum(int[] nums, int target)
 	{
-		for(int i = 0; i < nums.length - 1; i++)
+		for(int i = 0; i < nums.length; i++)
 		{
-			if(nums[i] + nums[i + 1] == target)
+			for(int j = i; j > nums.length; j++)
 			{
-				return new int[] {i,j};
+				if(nums[i] + nums[j] == target)
+				{
+					return new int[] {i,j};
+				}
 			}
 		}
+
+		throw new IllegalArgumentException("no match found");
 	}
 
 	public static void main(String[] args)
